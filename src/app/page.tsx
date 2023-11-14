1be9c1f23fa78cb5266b45a7170219e5569e2d83
guadasandoval <guadastoop@gmail.com>
2023-11-14 08:32:48 -0300

    img readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import loadImg from "../../public/assets/static/loadImage.svg"
 import logo from '../../public/assets/static/logo.svg'
+import eyepsw from '../../public/assets/static/eyepsw.svg'
 import agente from "../../public/assets/static/inicia-sesión-agente 2.png"
 import { useState } from 'react';
 
@@ -32,22 +33,22 @@ export default function Home() {
         alert('Por favor, ingresa un nombre y un correo electrónico válido.');
         return;
       }
-      console.log('Datos del formulario:', formData);
     };
   
   return (
     <main className="flex min-h-screen flex-col items-row justify-between bg-white box-border">
   <div className="flex min-h-full flex-row justify-between">
-    <div className="flex w-2/3 flex-col justify-center">
-      <div id="logo">
+    <div className="flex w-1/2 flex-col  justify-items-stretch">
+    <header className="flex w-full flex-col justify-stretch">
+    <div id="logo">
       <Image src={logo} alt="logo de la aplicacion Flexy" width={134} className="my-7"></Image>
         <hr className='text-gray-500'/>
       </div> 
+    </header>
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
       <h2 className="mt-10 text-left text-3xl font-bold leading-9 tracking-tight text-gray-900">¡Bienvenido!</h2>
       <p className="text-gray-500 text-base">Convertite ahora en un agente Flexy.</p>
     </div>
- 
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form className="space-y-6" onSubmit={handleSubmit}> 
        <div className=" flex mt-5 items-center">
@@ -73,9 +74,12 @@ export default function Home() {
             className="block w-full rounded-md border-0 py-1.5 pl-4 text-gray-900 bg-indigo-50 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2  sm:text-sm sm:leading-6"/>
           </div>
           <div className="mt-6">
-            <input id="password" name="password" type="password" minLength={8} required placeholder="Ingresá tu contraseña"
+            <div className='flex items-center'>
+               <input id="password" name="password" type="password" minLength={8} required placeholder="Ingresá tu contraseña"
              value={formData.password} onChange={handleChange} 
-            className="block w-full rounded-md border-0 py-1.5 pl-4 text-gray-900 bg-indigo-50 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2  sm:text-sm sm:leading-6"/>
+            className="block w-full rounded-md border-0 py-1.5 pl-4 z-0 text-gray-900 bg-indigo-50 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2  sm:text-sm sm:leading-6"/>
+            {/* <Image src={eyepsw} alt="" width={10} className='z-10'></Image> */}
+            </div>
             <p className="text-center text-gray-500 text-sm mt-3">Debe tener al menos 8 caracteres.</p>
           </div>
         </div>
